da6c84fa4587e36f5075fee792ce2604ab012975
Jastine Formentera <enitsajformentera.13@gmail.com>
2024-10-07 21:35:15 +0800

    initial setup

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+const greet = (name) => {
+    return `Hello, ${name}! Welcome to Node.js with TypeScript`;
+};
+console.log(greet("User"));
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+const greet = (name: string): string => {
+    return `Hello, ${name}! Welcome to Node.js with TypeScript`;
+  };
+  
+  console.log(greet("User"));
+  
